44ffec3586c2930362b4b9f26479178f3a0c700d
kousei3515 <yawara3515tanaka@gmail.com>
2019-11-16 14:49:54 +0900

    board編集

--- board.php
+++ board.php
@@ -67,7 +67,7 @@ $commentList = $stmt->fetchAll();
 <header>
     <div>
         <a href="/board/index.php">TOP</a>
-        <a href="/board/create_board.php">掲示板作成</a>
+        <a href="/board/create board.php">掲示板作成</a>
         <a href="/board/register.php">新規作成</a>
         <a href="/board/login.php">ログイン</a>
         <a href="/board/logout.php">ログアウト</a>
